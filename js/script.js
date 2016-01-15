3ac47c36658da6ee586452f6634ec4350b500ef4
Ian Paterson <ianpaterson@Ians-MacBook-Pro.local>
2016-01-15 18:43:08 -0500

    a bunch of refactoring, cause damn this code was ugly

--- js/script.js
+++ js/script.js
@@ -1,28 +1,69 @@
+$(function() {
+  var game = new Game(); 
 
-// Declare all necessary variables
-	var shuffleSpeed = 200;
-	var speed = 175;
-	var $start = $("#start4");
-	var $p1 = $("#p1");
-	var $p2 = $("#p2");
-	var $p3 = $("#p3");
-	var $p4 = $("#p4");
-	var $p5 = $("#p5");
-	var $p6 = $("#p6");
-	var $p7 = $("#p7");
-	var $p8 = $("#p8");
-	var $p9 = $("#p9");
-	var $p10 = $("#p10");
-	var $p11 = $("#p11");
-	var $p12 = $("#p12");
-	var $p13 = $("#p13");
-	var $p14 = $("#p14");
-	var $p15 = $("#p15");
-	var $p16 = $("#p16");
-	var $row2 = $("#row2");
-	var $row3 = $("#row3");
-	var $row4 = $("#row4");
-	var numMoves = 0;
+  $("#play-button").click(function(){
+    game.displayDifficultyPrompt($(this));
+  });
+
+  $(".prompt-button").click(function(){
+    game.addDifficultyLevel($(this));
+  });
+});
+
+var Game = function() {
+  this.numMoves = 0;
+  this.shuffleSpeed = 200;
+  this.speed = 175;
+  this.$puzzle = {
+    start: $("#start4"),
+    p1: $("#p1"),
+    p2: $("#p2"),
+    p3: $("#p3"),
+    p4: $("#p4"),
+    row2: $("#row2"),
+    p5: $("#p5"),
+    p6: $("#p6"),
+    p7: $("#p7"),
+    p8: $("#p8"),
+    row3: $("#row3"),
+    p9: $("#p9"),
+    p10: $("#p10"),
+    p11: $("#p11"),
+    p12: $("#p12"),
+    row3: $("#row4"),
+    p13: $("#p13"),
+    p14: $("#p14"),
+    p15: $("#p15"),
+    p16: $("#p16"),
+  };
+};
+
+Game.prototype.displayDifficultyPrompt = function($button) {
+  $button.prop('disabled', true);
+
+  // Display custom prompt asking user for difficulty level
+  $("#difficulty-prompt").show();
+};
+
+Game.prototype.addDifficultyLevel = function($button) {
+  var userChoice = $button.attr("id");
+
+  // Remove the prompt from the screen
+  $("#difficulty-prompt").hide();
+
+  // Compute the number of shuffles based on user's choice
+  switch (userChoice) {
+    case 'easy':
+      this.numMoves = 3;
+      break;
+    case 'medium':
+      this.numMoves = 20;
+      break;
+    case 'hard':
+      this.numMoves = 75;
+      break;
+  }
+};
 
 var leftMove = function(n, o) {
 	$(n).animate({left: "-=181px"},speed);
@@ -37,7 +78,7 @@ var rightMove = function(n, o) {
 };
 
 var upDown = function(n, o, val1, val2) {
-	$(n).animate({top: val1},speed);
+  $(n).animate({top: val1},speed);
 	$(o).animate({top: val2},speed);
 	$(n).after('<p id="mark"></p>');
 	$(o).after('<p id="mark2"></p>');
@@ -51,41 +92,6 @@ var upDown = function(n, o, val1, val2) {
 var replaceButton = function(name, replaceText){
 	$(name).after(replaceText);
 	$(name).remove();
-};
-
-var buildPuzzle = function(keySpot){
-
-	// Build the puzzle
-	$("#puzzle").empty();
-	for(var j = 2; j < 6; j++) {
-		if (j === 2) {
-			for(var i = 0; i < 5; i++) {
-				$("#puzzle").append('<div id="start' + i + '"></div>');
-			}
-		}					
-		for(var i = 1; i < 5; i++) {
-			if (j === 2) {
-			$("#puzzle").append('<div class="piece" id="p' + i + '"></div>');
-			}
-			else if (j === 3) {
-			$("#puzzle").append('<div class="piece" id="p' + (i+4) + '"></div>');
-			}
-			else if (j === 4) {
-			$("#puzzle").append('<div class="piece" id="p' + (i+8) + '"></div>');
-			}
-			else if (j === 5) {
-			$("#puzzle").append('<div class="piece" id="p' + (i+12) + '"></div>');
-			}
-		}
-		if (j !== 5) {
-			$("#puzzle").append('<div id="row' + j + '"></div>');
-		}
-		else {
-			for(var i = 0; i < 5; i++) {
-				$("#puzzle").append('<div id="end' + i + '"></div>');
-			}
-		}
-	}
 };
 
 // Create a function to animate a sequence once the user completes the puzzle
@@ -115,36 +121,8 @@ var randomNum = function(highValue, lowValue){
 };
 
 var game = function(){
-
-	// When shuffle button pressed
-	$("#button").click(function(){
-
-		// Call replaceButton to disable PLAY button
-		replaceButton(this, '<div id="button3"><p id="button-disabled-text">PLAY</p></div>');
-
-		// Display custom prompt asking user for difficulty level
-		$("#puzzle").append('<div id="difficulty-prompt">' +
-					'<p id="prompt-text">Choose a Difficulty Level:</p>' +
-					'<div class="prompt-button" id="easy"><p class="prompt-button-text">EASY</p></div>' +
-					'<div class="prompt-button" id="medium"><p class="prompt-button-text">MEDIUM</p></div>' +
-					'<div class="prompt-button" id="hard"><p class="prompt-button-text">HARD</p></div></div>');
-
 		// When diffculty level is clicked
-		$(".prompt-button").click(function(){
 
-			// Remove the prompt from the screen
-			$("#difficulty-prompt").remove();
-
-			// Create an if statement to compute the number of shuffles based on user's choice
-			if ($(this).attr("id") === "easy") {
-				numMoves = 3;
-			}
-			else if ($(this).attr("id") === "medium") {
-				numMoves = 20;
-			}
-			else if ($(this).attr("id") === "hard") {
-				numMoves = 75;
-			}
 
 			// Replace the 10th tile with an empty slot
 			$("#p10").after('<div id="empty"></div>');
@@ -155,14 +133,11 @@ var game = function(){
 			var boardMove = function(name){
 				if ($(name).prev().attr("id") === "empty") {
 				 	leftMove(name, $empty);
-					}
-				else if ($(name).prevUntil("#empty").length === 4) {
+				} else if ($(name).prevUntil("#empty").length === 4) {
 					upDown(name, $empty, "-=130px", "+=130px");
-				}
-				else if ($(name).next().attr("id") === "empty") {
+				} else if ($(name).next().attr("id") === "empty") {
 					rightMove(name, $empty);
-				}
-				else if ($(name).nextUntil("#empty").length === 4) {
+				} else if ($(name).nextUntil("#empty").length === 4) {
 					upDown(name, $empty, "+=130px", "-=130px");
 				}
 			};
@@ -170,27 +145,27 @@ var game = function(){
 			// Create an array of 16 numbers
 			var pieceNum = new Array(16);
 	    	
-	    	// Fill array with numbers from 1-16
-	    	for(var i = 0; i < pieceNum.length; i++) {
-	    		pieceNum[i] = i+1;
-	    	}
-	    	
-	    	// Sort array in random order
-	    	for(var i = 0; i < pieceNum.length; i++) {
-	    		
-	    		// Call randomNum and initialize randomIndex to it
-	    		var randomIndex = randomNum(15, 0);
-	    		
-	    		// Swap the two elements if the indexes are not equal
-	    		if(pieceNum[i] !== pieceNum[randomIndex]) {
-	    			var hold = pieceNum[i];
-	    			pieceNum[i] = pieceNum[randomIndex];
-	    			pieceNum[randomIndex] = hold;
-	    		}
-	    	}
+      // Fill array with numbers from 1-16
+      for(var i = 0; i < pieceNum.length; i++) {
+        pieceNum[i] = i+1;
+      }
+      
+      // Sort array in random order
+      for(var i = 0; i < pieceNum.length; i++) {
+        
+        // Call randomNum and initialize randomIndex to it
+        var randomIndex = randomNum(15, 0);
+        
+        // Swap the two elements if the indexes are not equal
+        if(pieceNum[i] !== pieceNum[randomIndex]) {
+          var hold = pieceNum[i];
+          pieceNum[i] = pieceNum[randomIndex];
+          pieceNum[randomIndex] = hold;
+        }
+      }
 
 	 		// Shuffle the tiles by calling boardMove(); in a nested for loop
-	    	for(var j = 0; j < numMoves; j++) {
+	    for(var j = 0; j < numMoves; j++) {
 				for(var i = 0; i < pieceNum.length; i++) {
 					var $piece = $("#p" + pieceNum[i]);
 					boardMove($piece);
@@ -198,7 +173,7 @@ var game = function(){
 			}
 	
 
-	// Create arrow key functionality
+	  // Create arrow key functionality
 		$(document).keydown(function(e) {
 				switch(e.which) {
 				// Left Arrow Key
@@ -264,9 +239,4 @@ var game = function(){
 					youWin();
 				});
 		});
-		});
-	});
 };
-
-buildPuzzle();
-game();
