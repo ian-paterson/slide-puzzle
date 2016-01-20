26efe74687dea10759019e67e3c67c4e38736362
Ian Paterson <ianpaterson@Ians-MacBook-Pro.local>
2016-01-20 18:56:50 -0500

    keep on keepin on

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function() {
   var game = new Game(); 
 
@@ -7,36 +9,40 @@ $(function() {
 
   $(".prompt-button").click(function(){
     game.addDifficultyLevel($(this));
+    game.start();
   });
+
+  console.log(game.puzzle[12]);
 });
 
 var Game = function() {
   this.numMoves = 0;
   this.shuffleSpeed = 200;
   this.speed = 175;
-  this.$puzzle = {
-    start: $("#start4"),
-    p1: $("#p1"),
-    p2: $("#p2"),
-    p3: $("#p3"),
-    p4: $("#p4"),
-    row2: $("#row2"),
-    p5: $("#p5"),
-    p6: $("#p6"),
-    p7: $("#p7"),
-    p8: $("#p8"),
-    row3: $("#row3"),
-    p9: $("#p9"),
-    p10: $("#p10"),
-    p11: $("#p11"),
-    p12: $("#p12"),
-    row3: $("#row4"),
-    p13: $("#p13"),
-    p14: $("#p14"),
-    p15: $("#p15"),
-    p16: $("#p16"),
-  };
+  this.puzzle = [
+    new Tile($("#start4")),
+    new Tile($("#p1")),
+    new Tile($("#p2")),
+    new Tile($("#p3")),
+    new Tile($("#p4")),
+    new Tile($("#row2")),
+    new Tile($("#p5")),
+    new Tile($("#p6")),
+    new Tile($("#p7")),
+    new Tile($("#p8")),
+    new Tile($("#row3")),
+    new Tile($("#p9")),
+    new Tile($("#p10")),
+    new Tile($("#p11")),
+    new Tile($("#p12")),
+    new Tile($("#row4")),
+    new Tile($("#p13")),
+    new Tile($("#p14")),
+    new Tile($("#p15")),
+    new Tile($("#p16"))
+  ];
 };
+
 
 Game.prototype.displayDifficultyPrompt = function($button) {
   $button.prop('disabled', true);
@@ -65,28 +71,71 @@ Game.prototype.addDifficultyLevel = function($button) {
   }
 };
 
-var leftMove = function(n, o) {
-	$(n).animate({left: "-=181px"},speed);
-	$(o).animate({left: "+=181px"},speed);
-	$(n).after(o);
+Game.prototype.start = function() {
+  this.removeTile();
+
+  $(".piece").click(function(){
+    var tile = new Tile(this);
+    tile.move();
+  });
 };
 
-var rightMove = function(n, o) {
-	$(n).animate({left: "+=181px"},speed);
-	$(o).animate({left: "-=181px"},speed);
-	$(n).before(o);
+Game.prototype.removeTile = function() {
+  this.puzzle[12].addClass('empty');
+  this.puzzle[12].removeClass('piece');
 };
 
-var upDown = function(n, o, val1, val2) {
-  $(n).animate({top: val1},speed);
-	$(o).animate({top: val2},speed);
-	$(n).after('<p id="mark"></p>');
-	$(o).after('<p id="mark2"></p>');
+Game.prototype.shuffleTiles = function() {
+  var puzzleCopy = this.puzzle;
+  shuffle(puzzleCopy);
+
+  for(var j = 0; j < numMoves; j++) {
+    puzzleCopy.forEach(function(tile) {
+      tile.move();
+    });
+  }
+};
+
+var Tile = function(identifier) {
+  this.element = identifier;
+};
+
+Tile.move = function() {
+  if (this.element.prev().attr("id") === "p10") {
+    this.leftMove(name, $empty);
+  } else if (this.element.prevUntil("#p10").length === 4) {
+    upDown(name, $empty, "-=130px", "+=130px");
+  } else if (this.element.next().attr("id") === "p10") {
+    rightMove(name, $empty);
+  } else if (this.element.nextUntil("#p10").length === 4) {
+    upDown(name, $empty, "+=130px", "-=130px");
+  }
+}
+
+Tile.leftMove = function() {
+  this.element.animate({left: "-=181px"},speed);
+  Game.emptyTile.element.animate({left: "+=181px"},speed);
+  this.element.after(o);
+};
+
+Tile.rightMove = function(n, o) {
+  this.element.animate({left: "+=181px"},speed);
+  Game.emptyTile.element.animate({left: "-=181px"},speed);
+  this.element.before(o);
+};
+
+Tile.upDown = function(val1, val2) {
+  this.element.animate({top: val1},speed);
+	Game.emptyTile.animate({top: val2},speed);
+	this.element.after('<p id="mark"></p>');
+	Game.emptyTile.after('<p id="mark2"></p>');
 	$("#mark").after(o);
 	$("#mark2").after(n);
 	$("#mark").remove();
 	$("#mark2").remove();
 };
+
+Game.emptyTile = new Tile($("#p10"));
 
 // Create function to replace button with another button (disabled,etc.)
 var replaceButton = function(name, replaceText){
@@ -114,6 +163,26 @@ var youWin = function() {
 			$("#sidebar").prepend('<p id="win-text">YOU WIN!</p>');
 			}
 };
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 	
 // Create function to return a random number
 var randomNum = function(highValue, lowValue){
@@ -121,27 +190,6 @@ var randomNum = function(highValue, lowValue){
 };
 
 var game = function(){
-		// When diffculty level is clicked
-
-
-			// Replace the 10th tile with an empty slot
-			$("#p10").after('<div id="empty"></div>');
-			$("#p10").remove();
-			var $empty = $("#empty");
-
-			// Create a function to move the tiles
-			var boardMove = function(name){
-				if ($(name).prev().attr("id") === "empty") {
-				 	leftMove(name, $empty);
-				} else if ($(name).prevUntil("#empty").length === 4) {
-					upDown(name, $empty, "-=130px", "+=130px");
-				} else if ($(name).next().attr("id") === "empty") {
-					rightMove(name, $empty);
-				} else if ($(name).nextUntil("#empty").length === 4) {
-					upDown(name, $empty, "+=130px", "-=130px");
-				}
-			};
-			
 			// Create an array of 16 numbers
 			var pieceNum = new Array(16);
 	    	
@@ -168,7 +216,6 @@ var game = function(){
 	    for(var j = 0; j < numMoves; j++) {
 				for(var i = 0; i < pieceNum.length; i++) {
 					var $piece = $("#p" + pieceNum[i]);
-					boardMove($piece);
 				}
 			}
 	
@@ -231,12 +278,4 @@ var game = function(){
 			youWin();
 		}); 
 			
-		// Call boardMove(); and apply it if the user clicks a tile
-		$(".piece").click(function(){
-			boardMove(this);
-			// Call youWin(); if the user's final mouse click is completion of the puzzle
-				$(".piece").click(function(){
-					youWin();
-				});
-		});
 };
